2eff9ac1fea5f86c2e80ebb78e0388ed2e16e07b
nurbolot <ngulamidinov@gmail.com>
2023-02-11 22:39:26 +0600

    add .travis.yml

---
+++ src/test/java/com/ntg/springredismvn/SpringRedisMvnApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ntg.springredismvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringRedisMvnApplicationTest {
+//    @Test
+//    void check() {
+//
+//    }
+}
